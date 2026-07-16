c52bd4c36e0cfb05b9d570627c5ca50b2c09df8d
Mark Raynsford <code@io7m.com>
2026-07-16 15:21:22 +0100

    Remove demo copy.

--- palace-r2-ts/make.sh
+++ palace-r2-ts/make.sh
@@ -7,8 +7,3 @@ npm run eslint-ts
 npm run build
 npm run test
 
-cp ../org.librarysimplified.r2.vanilla/src/main/resources/org/librarysimplified/r2/vanilla/readium/scripts/crypto-sha256.js dist/
-cp ../org.librarysimplified.r2.vanilla/src/main/resources/org/librarysimplified/r2/vanilla/readium/scripts/highlight.js dist/
-cp ../org.librarysimplified.r2.vanilla/src/main/resources/org/librarysimplified/r2/vanilla/readium/readium-css/ReadiumCSS-before.css dist/
-cp ../org.librarysimplified.r2.vanilla/src/main/resources/org/librarysimplified/r2/vanilla/readium/readium-css/ReadiumCSS-after.css dist/
-cp demo/index.xhtml dist/

9a08d0f97e0d065c313fec9dabb0b38720945cc1
Mark Raynsford <code@io7m.com>
2026-08-25 13:39:29 +0100

    Make unpacking script safer by using two directory roots.

--- palace-r2-ts/make-mathjax.sh
+++ palace-r2-ts/make-mathjax.sh
@@ -7,9 +7,12 @@ sha256sum -c "mathjax-newcm-font-${MATHJAX_VERSION}.tgz.sha256"
 wget -c "https://registry.npmjs.org/mathjax/-/mathjax-${MATHJAX_VERSION}.tgz"
 sha256sum -c "mathjax-${MATHJAX_VERSION}.tgz.sha256"
 
+rm -rf mathjax-unpack-font
+mkdir -p mathjax-unpack-font
+tar -C mathjax-unpack-font -x -v -f "mathjax-newcm-font-${MATHJAX_VERSION}.tgz"
+
 rm -rf mathjax-unpack
 mkdir -p mathjax-unpack
-tar -C mathjax-unpack -x -v -f "mathjax-newcm-font-${MATHJAX_VERSION}.tgz"
 tar -C mathjax-unpack -x -v -f "mathjax-${MATHJAX_VERSION}.tgz"
 
 rm -rf mathjax-dist
@@ -17,8 +20,8 @@ mkdir -p mathjax-dist
 mkdir -p mathjax-dist/fonts/mathjax-newcm-font/chtml/woff2
 mkdir -p mathjax-dist/fonts/mathjax-newcm-font/chtml/dynamic
 
-cp mathjax-unpack/package/chtml/woff2/*.woff2 mathjax-dist/fonts/mathjax-newcm-font/chtml/woff2/
-cp mathjax-unpack/package/chtml/dynamic/*.js  mathjax-dist/fonts/mathjax-newcm-font/chtml/dynamic/
+cp mathjax-unpack-font/package/chtml/woff2/*.woff2 mathjax-dist/fonts/mathjax-newcm-font/chtml/woff2/
+cp mathjax-unpack-font/package/chtml/dynamic/*.js  mathjax-dist/fonts/mathjax-newcm-font/chtml/dynamic/
 cp mathjax-unpack/package/mml-chtml.js        mathjax-dist/mml-chtml.js
 
 rsync -avz --delete mathjax-dist/ ../org.librarysimplified.r2.vanilla/src/main/resources/org/librarysimplified/r2/vanilla/readium/mathjax/

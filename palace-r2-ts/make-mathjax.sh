7087ad45929d683592939b83ee8821094e4ed1db
Mark Raynsford <code@io7m.com>
2026-08-25 13:06:10 +0100

    Unpack mathjax.

---
+++ palace-r2-ts/make-mathjax.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -ex
+
+MATHJAX_VERSION=4.1.3
+
+wget -c "https://registry.npmjs.org/@mathjax/mathjax-newcm-font/-/mathjax-newcm-font-${MATHJAX_VERSION}.tgz"
+sha256sum -c "mathjax-newcm-font-${MATHJAX_VERSION}.tgz.sha256"
+
+rm -rf mathjax-unpack
+mkdir -p mathjax-unpack
+tar -C mathjax-unpack -x -v -f "mathjax-newcm-font-${MATHJAX_VERSION}.tgz"
+
+rm -rf mathjax-dist
+mkdir -p mathjax-dist
+mkdir -p mathjax-dist/fonts/mathjax-newcm-font/chtml/woff2
+mkdir -p mathjax-dist/fonts/mathjax-newcm-font/chtml/dynamic
+
+cp ./MathJax-src/components/mjs/mml-chtml/mml-chtml.js mathjax-dist/
+cp mathjax-unpack/package/chtml/woff2/*.woff2          mathjax-dist/fonts/mathjax-newcm-font/chtml/woff2/
+cp mathjax-unpack/package/chtml/dynamic/*.js           mathjax-dist/fonts/mathjax-newcm-font/chtml/dynamic/
+
